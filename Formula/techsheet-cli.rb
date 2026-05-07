44755f7a7f33246514cbee4dd16b013cd5d7ee58
Schwurbl Supreme <schwurbl-supreme@proton.me>
2026-05-07 10:56:45 +0200

    Updated formula

---
+++ Formula/techsheet-cli.rb
@@ -0,0 +1,15 @@
+class TechsheetCli < Formula
+  desc "TechSheet.org CLI tool"
+  homepage "https://github.com/techsheet/techsheet"
+  url "https://github.com/techsheet/techsheet/releases/download/v1.2.0/techsheet-macos-arm64"
+  sha256 "93ac0a39c8b4773f06f3f94ddde841925cb550a515174b655da57ca5f25522d0"
+  license "Apache-2.0"
+
+  def install
+    bin.install "techsheet-macos-arm64" => "techsheet"
+  end
+
+  test do
+    system bin/"techsheet", "--version"
+  end
+end
