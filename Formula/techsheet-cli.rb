b5ec96da92825eb07df71280695980d4a6cfcad5
Schwurbl Supreme <schwurbl-supreme@proton.me>
2026-05-08 16:39:08 +0200

    Wow, brew is really one of the worst package managers I know

--- Formula/techsheet-cli.rb
+++ Formula/techsheet-cli.rb
@@ -9,11 +9,6 @@ class TechsheetCli < Formula
       sha256 "3dd95d7359966eadedd635d891051f58e40c935a0c88ce8cc0b0ce2567672b13"
       version "1.3.0"
     end
-    on_intel do
-      url "https://github.com/techsheet/techsheet/releases/download/v1.3.0/techsheet-macos-arm64"
-      sha256 "3dd95d7359966eadedd635d891051f58e40c935a0c88ce8cc0b0ce2567672b13"
-      version "1.3.0"
-    end
   end
 
   on_linux do
