6eea398607a8b10f8cb59e63d7eac77f9aee3bc3
Schwurbl Supreme <schwurbl-supreme@proton.me>
2026-05-08 12:35:06 +0200

    Fix brew style

--- Formula/techsheet-cli.rb
+++ Formula/techsheet-cli.rb
@@ -2,9 +2,9 @@ class TechsheetCli < Formula
   desc "TechSheet.org CLI tool"
   homepage "https://github.com/techsheet/techsheet"
   url "https://github.com/techsheet/techsheet/releases/download/v1.2.1/techsheet-macos-arm64"
+  version "1.2.1"
   sha256 "6ed5cbc2e0d902dd705cfbcccb805ce62022f273de8c14f807089888a2fae6a0"
   license "Apache-2.0"
-  version "1.2.1"
 
   def install
     bin.install "techsheet-macos-arm64" => "techsheet"
