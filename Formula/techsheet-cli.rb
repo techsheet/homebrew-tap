2b68bfe0af40e169036a86dd18753d4bb33ce312
Schwurbl Supreme <schwurbl-supreme@proton.me>
2026-05-08 12:28:10 +0200

    Add explicit version to formula

--- Formula/techsheet-cli.rb
+++ Formula/techsheet-cli.rb
@@ -4,6 +4,7 @@ class TechsheetCli < Formula
   url "https://github.com/techsheet/techsheet/releases/download/v1.2.0/techsheet-macos-arm64"
   sha256 "93ac0a39c8b4773f06f3f94ddde841925cb550a515174b655da57ca5f25522d0"
   license "Apache-2.0"
+  version "1.2.0"
 
   def install
     bin.install "techsheet-macos-arm64" => "techsheet"
