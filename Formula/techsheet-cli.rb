a5b390734a3cf64b1b0b8bff06b5d12ae8bb13bf
Frank Neff <frne@users.noreply.github.com>
2026-05-08 11:33:16 +0000

    techsheet-cli 1.3.0
    # Conflicts:
    #	Formula/techsheet-cli.rb

--- Formula/techsheet-cli.rb
+++ Formula/techsheet-cli.rb
@@ -1,30 +1,13 @@
 class TechsheetCli < Formula
   desc "TechSheet.org CLI tool"
   homepage "https://github.com/techsheet/techsheet"
+  url "https://github.com/techsheet/techsheet/releases/download/v1.3.0/techsheet-macos-arm64"
+  sha256 "3dd95d7359966eadedd635d891051f58e40c935a0c88ce8cc0b0ce2567672b13"
   license "Apache-2.0"
-
-  on_macos do
-    on_arm do
-      url "https://github.com/techsheet/techsheet/releases/download/v1.2.1/techsheet-macos-arm64"
-      sha256 "6ed5cbc2e0d902dd705cfbcccb805ce62022f273de8c14f807089888a2fae6a0"
-      version "1.2.1"
-    end
-  end
-
-  on_linux do
-    on_intel do
-      url "https://github.com/techsheet/techsheet/releases/download/v1.2.1/techsheet-linux-x64"
-      sha256 "6bb3f0594c9cb1c95a98a3fb0b636a94e334c3f1bee7d5efe792c32067ef36d3"
-      version "1.2.1"
-    end
-  end
+  version "1.3.0"
 
   def install
-    if OS.mac?
-      bin.install "techsheet-macos-arm64" => "techsheet"
-    elsif OS.linux?
-      bin.install "techsheet-linux-x64" => "techsheet"
-    end
+    bin.install "techsheet-macos-arm64" => "techsheet"
   end
 
   test do
